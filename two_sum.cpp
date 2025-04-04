3559675aea4bb325e617dcaf504d9044c3005520
Digvijay-Parmar <digvijaysinhparmar2804@gmail.com>
2025-04-04 17:50:06 +0530

    Add files via upload

---
+++ two_sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+    
+            unordered_map<int,int>m;
+            for(int i=0;i<nums.size();i++){
+                m[nums[i]]=i;
+            }
+            
+            for(int i=0;i,nums.size();i++){
+                int other=target-nums[i];
+    
+                if(m.find(other)!=m.end() && m[other]!=i){
+                    return{i,m[other]};
+                }
+            }
+            return {};
+        }
+    };
